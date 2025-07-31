5036044bc7c2f07a975233d8d128d78355827e6f
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-07-31 19:04:38 +0300

    Create an object and function for performing math operations

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let operation = {
+  firstOperand: "",
+  operator: "",
+  secondOperand: "",
+};
+
+function perform(operation) {
+  switch (operation.operator) {
+    case "+":
+      return add(+operation.firstOperand, +operation.secondOperand);
+    case "-":
+      return subtract(+operation.firstOperand, +operation.secondOperand);
+    case "*":
+      return multiply(+operation.firstOperand, +operation.secondOperand);
+    case "/":
+      return divide(+operation.firstOperand, +operation.secondOperand);
+  }
+}
